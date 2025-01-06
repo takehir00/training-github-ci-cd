b8140243e183ea43a2acac611b7c59201c3d352f
okuyama <>
2025-01-06 10:03:57 +0900

    リリース自動 go

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,9 +1,9 @@
 package main
 
-func EvenOrOdd(number int) string {
-    if number%2 == 0 {
-        return "isEven"
-    } else {
-        return "isOdd"
-    }
+import "fmt"
+
+var version string // ビルド時にldflagsフラグ経由でバージョンを埋め込むための変数
+
+func main() {
+    fmt.Printf("Example %s\n", version)
 }
